b192ce09a1368b1e1c37a9dc7c542711c12a3f21
developer's Brasil DBA <dnadevnet@gmail.com>
2022-09-11 23:47:38 -0300

    Datpicker

--- frontend/src/components/salesCard/index.tsx
+++ frontend/src/components/salesCard/index.tsx
@@ -1,77 +1,88 @@
-import NotificationButton from '../notificatioButton/Index';
-import './card.css';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import NotificationButton from "../notificatioButton/Index";
+import "./card.css";
 
-
-function SalesCard(){
-  return(
+function SalesCard() {
+  return (
     <div className="sales-card">
-        <h2 className="sales-title">Vendas</h2>
-        <div className="sales-form">
-          <div className="sales-form-item">
-            <input type="text" />
-          </div>
-          <div className="sales-form-item">
-            <input type="text" />
-          </div>
+      <h2 className="sales-title">Vendas</h2>
+      <div className="sales-form">
+        <div className="sales-form-item">
+          <DatePicker
+            selected={new Date()}
+            onChange={(date: Date) => {}}
+            className="dsmeta-form-control"
+            dateFormat="dd/MM/yyyy"
+          />
         </div>
-        <div>
-          <table className="sales-table">
-            <thead className="sales-table-header">
-              <tr className="sales-table-title">
-                <th className="secund">ID</th>
-                <th className="first">Data</th>
-                <th>Vendedor</th>
-                <th className="secund">Visitas</th>
-                <th className="secund">Vendas</th>
-                <th>Total</th>
-                <th>Notificar</th>
-              </tr>  
-            </thead>
-            <tbody className="sales-table-content">
-              <tr className="sales-table-item">
-                <td className="secund">#341</td>
-                <td className="first">08/07/2022</td>
-                <td>AnaKim</td>
-                <td className="secund">15</td>
-                <td className="secund">11</td>
-                <td>R$ 53300.00</td>
-                <td>
-                    <div className="sales-btn">
-                     <NotificationButton />
-                    </div>
-                 </td>
-              </tr>
-              <tr className="sales-table-item">
-                <td className="secund">#341</td>
-                <td className="first">08/07/2022</td>
-                <td>AnaKim</td>
-                <td className="secund">15</td>
-                <td className="secund">11</td>
-                <td>R$ 53300.00</td>
-                <td>
-                  <div className="sales-btn">
-                  <NotificationButton />
-                  </div>
-                </td>
-              </tr>
-              <tr className="sales-table-item">
-                <td className="secund">#341</td>
-                <td className="first">08/07/2022</td>
-                <td>AnaKim</td>
-                <td className="secund">15</td>
-                <td className="secund">11</td>
-                <td>R$ 53300.00</td>
-                <td>
-                  <div className="sales-btn">
-                  <NotificationButton />
-                </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+        <div className="sales-form-item">
+          <DatePicker
+            selected={new Date()}
+            onChange={(date: Date) => {}}
+            className="dsmeta-form-control"
+            dateFormat="dd/MM/yyyy"
+          />
         </div>
       </div>
-  )
+      <div>
+        <table className="sales-table">
+          <thead className="sales-table-header">
+            <tr className="sales-table-title">
+              <th className="secund">ID</th>
+              <th className="first">Data</th>
+              <th>Vendedor</th>
+              <th className="secund">Visitas</th>
+              <th className="secund">Vendas</th>
+              <th>Total</th>
+              <th>Notificar</th>
+            </tr>
+          </thead>
+          <tbody className="sales-table-content">
+            <tr className="sales-table-item">
+              <td className="secund">#341</td>
+              <td className="first">08/07/2022</td>
+              <td>AnaKim</td>
+              <td className="secund">15</td>
+              <td className="secund">11</td>
+              <td>R$ 53300.00</td>
+              <td>
+                <div className="sales-btn">
+                  <NotificationButton />
+                </div>
+              </td>
+            </tr>
+            <tr className="sales-table-item">
+              <td className="secund">#341</td>
+              <td className="first">08/07/2022</td>
+              <td>AnaKim</td>
+              <td className="secund">15</td>
+              <td className="secund">11</td>
+              <td>R$ 53300.00</td>
+              <td>
+                <div className="sales-btn">
+                  <NotificationButton />
+                </div>
+              </td>
+            </tr>
+            <tr className="sales-table-item">
+              <td className="secund">#341</td>
+              <td className="first">08/07/2022</td>
+              <td>AnaKim</td>
+              <td className="secund">15</td>
+              <td className="secund">11</td>
+              <td>R$ 53300.00</td>
+              <td>
+                <div className="sales-btn">
+                  <NotificationButton />
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default SalesCard
+export default SalesCard;
